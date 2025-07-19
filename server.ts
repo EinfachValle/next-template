8a92fb09e9bce191ad858367a6330ed5f8b699ac
Valentin Roehle <roehlevalentin@gmail.com>
2025-07-19 17:40:46 +0200

    Refactor code structure and remove redundant sections for improved readability and maintainability

---
+++ server.ts
@@ -0,0 +1,1 @@
+// Maybe external backend
---
+++ src/utils/stylingUtils.ts
@@ -0,0 +1,34 @@
+"use client";
+
+import { THEME } from "../constants/generalConstants";
+
+export const applicationScrollbar = (theme) => {
+  const isDarkTheme = theme.palette.mode === THEME.DARK;
+
+  return {
+    "&::-webkit-scrollbar": {
+      width: 4,
+      height: 4,
+      borderRadius: 4,
+    },
+    "&::-webkit-scrollbar-track": {
+      WebkitBoxShadow: "inset 0 0 6px rgba(0,0,0,0.00)",
+      backgroundColor: isDarkTheme ? "transparent" : "#FAFAFA",
+    },
+    "&::-webkit-scrollbar-thumb": {
+      borderRadius: "4px",
+      backgroundColor: isDarkTheme ? "#4DBFC3" : "#CFD5DE",
+      "&:hover": {
+        backgroundColor: isDarkTheme ? "#82D7DA" : "transparent",
+      },
+    },
+  };
+};
+
+export const capitalizeFirstLetter = (s: string) => {
+  if (typeof s !== "string") return "";
+
+  const lowerCased = s.toLowerCase();
+
+  return lowerCased.charAt(0).toUpperCase() + lowerCased.slice(1);
+};
